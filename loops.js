4e29ef5996d26af6420603736a9c632c513dbe41
chrismadsen2396@gmail.com <chrismadsen2396@gmail.com>
2018-03-02 19:17:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,10 +17,12 @@ function whileLoop(n) {
 return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
 function doWhileLoop(array) {
   do {
     
-  } while {
-    maybeTrue()
-  }
+  } while array.length > 0 && (maybeTrue());
 }
